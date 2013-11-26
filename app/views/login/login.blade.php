fa5ca602ea5afbc897fb674b4d8d0c9e8d8f06cb
jroog <j.roogeer@gmail.com>
2013-11-26 16:45:39 -0500

    26/11/2013 -- (Cambio View login)

--- app/views/login/login.blade.php
+++ app/views/login/login.blade.php
@@ -1,131 +1,67 @@
 <!DOCTYPE html>
-<!--[if lt IE 7 ]> <html lang="en" class="no-js ie6 lt8"> <![endif]-->
-<!--[if IE 7 ]>    <html lang="en" class="no-js ie7 lt8"> <![endif]-->
-<!--[if IE 8 ]>    <html lang="en" class="no-js ie8 lt8"> <![endif]-->
-<!--[if IE 9 ]>    <html lang="en" class="no-js ie9"> <![endif]-->
-<!--[if (gt IE 9)|!(IE)]><!--> 
-<html lang="es" class="no-js"> <!--<![endif]-->
-    <head>
-        <meta charset="UTF-8" />
-        <!-- <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">  -->
-        <title>LP Analistas y Consultores</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
-        <meta name="description" content="Login and Registration Form with HTML5 and CSS3" />
-        <meta name="keywords" content="html5, css3, form, switch, animation, :target, pseudo-class" />
-        <meta name="author" content="Codrops" />
-        <link rel="shortcut icon" href="../favicon.ico"> 
-        <link rel="stylesheet" type="text/css" href="css/loginCSS/demo.css" />
-        <link rel="stylesheet" type="text/css" href="css/loginCSS/style3.css" />
-		<link rel="stylesheet" type="text/css" href="css/loginCSS/animate-custom.css" />
-    </head>
-    <body>
-        <div class="container">
-            <!-- Codrops top bar -->
-            <div class="codrops-top">
-                <a href="">
-                    <!-- <strong>&laquo; Previous Demo: </strong>Responsive Content Navigator -->
-                </a>
-                <span class="right">
-                   <!--  <a href=" http://tympanus.net/codrops/2012/03/27/login-and-registration-form-with-html5-and-css3/">
-                        <strong>Back to the Codrops Article</strong>
-                    </a> -->
-                </span>
-                <div class="clr"></div>
-            </div><!--/ Codrops top bar -->
-            <header>
-                <h1>Login LP Analistas y Consultores 
-                    <span>Usuarios</span>
-                </h1>
-				<!-- <nav class="codrops-demos">
-					<span>Click <strong>"Join us"</strong> to see the form switch</span>
-					<a href="index.html" class="current-demo">Demo 1</a>
-					<a href="index2.html">Demo 2</a>
-					<a href="index3.html">Demo 3</a>
-				</nav> -->
-            </header>
-            <section>
 
-            {{Form::open(array('url'=>'/login'))}}
+<html lang="es">
+<head>
+    <meta charset="UTF-8" />
+    <title>LP Analistas y Consultores</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
+    <meta name="description" content="Login de usuarios LP Analistas" />
+    <meta name="keywords" content="LP, LP Analistas, Sistema LP, Login, usuarios, lp usuarios" />
+    <meta name="author" content="LP Analistas" />
 
-                <div id="container_demo" >
-                    <!-- hidden anchor to stop jump http://www.css3create.com/Astuce-Empecher-le-scroll-avec-l-utilisation-de-target#wrap4  -->
-                    <a class="hiddenanchor" id="toregister"></a>
-                    <a class="hiddenanchor" id="tologin"></a>
-                    <div id="wrapper">
-                        <div id="login" class="animate form">
-                            <form  action="mysuperscript.php" autocomplete="on"> 
-                                <h1>Log in</h1> 
-                                <p> 
-                                    <label for="username" class="uname" data-icon="u" > DNI </label>
-                                    {{Form::text('username','',array('class'=>'uname','required'=>'required','placeholder'=>'Ingrese DNI'))}}
-                                   <!--
-                                    <input id="username" name="username" type="text" required="required" placeholder="Ingrese DNI"/>
-                                    -->
-                                </p>
-                                <p> 
-                                    <label for="password" class="youpasswd" data-icon="p"> Password </label>
+    <link rel="stylesheet" href="css/loginCSS/styleLogin.css">
+    <link rel="shortcut icon" type="image/x-icon" href="favi2.ico">
+</head>
 
-                                      {{Form::password('password','',array('name'=>'password','class'=>'uname','required'=>'required','laceholder'=>'Ingrese clave'))}}   
-                                    <!--
-                                    <input id="password" name="password" required="required" type="password" placeholder="Password" /> 
-
-                                    -->
-                                </p>
-                                <p class="keeplogin"> 
-									<!-- <input type="checkbox" name="loginkeeping" id="loginkeeping" value="loginkeeping" /> 
-									<label for="loginkeeping">Keep me logged in</label> -->
-								</p>
-                                <p class="login button"> 
-                                   <!--
-                                    <input type="submit" value="Login" /> 
-                                    -->
-                                 {{Form::submit('Ingresar', array('class' => 'btn btn-default'))}}
-
-
-								</p>
-                                <p class="change_link">
-									Registar Nuevo Usuario?
-									<a href="#toregister" class="to_register">Nuevo</a>
-								</p>
-                            </form>
-                        </div>
-
-                        <div id="register" class="animate form">
-                            <form  action="mysuperscript.php" autocomplete="on"> 
-                                <h1> Sign up </h1> 
-                                <p> 
-                                    <label for="usernamesignup" class="uname" data-icon="u">Your username</label>
-                                    <input id="usernamesignup" name="usernamesignup" required="required" type="text" placeholder="mysuperusername690" />
-                                </p>
-                                <p> 
-                                    <label for="emailsignup" class="youmail" data-icon="e" > Your email</label>
-                                    <input id="emailsignup" name="emailsignup" required="required" type="email" placeholder="mysupermail@mail.com"/> 
-                                </p>
-                                <p> 
-                                    <label for="passwordsignup" class="youpasswd" data-icon="p">Your password </label>
-                                    <input id="passwordsignup" name="passwordsignup" required="required" type="password" placeholder="eg. X8df!90EO"/>
-                                </p>
-                                <p> 
-                                    <label for="passwordsignup_confirm" class="youpasswd" data-icon="p">Please confirm your password </label>
-                                    <input id="passwordsignup_confirm" name="passwordsignup_confirm" required="required" type="password" placeholder="eg. X8df!90EO"/>
-                                </p>
-                                <p class="signin button"> 
-									<input type="submit" value="Sign up"/> 
-								</p>
-                                <p class="change_link">  
-									Already a member ?
-									<a href="#tologin" class="to_register"> Go and log in </a>
-								</p>
-                            </form>
-                        </div>
-						
-                    </div>
-                </div>  
-
-                {{Form::close()}}
-
-
+<body>
+    <header>
+        <section id="top-bar">
+            <a href="http://www.lpanalistas.com/" class="button">
+            Home Page LP
+            </a>
+        </section>
+        <h1>
+            <section class="login">
+                LOGIN DE USUARIO
             </section>
-        </div>
-    </body>
+            <a href="http://www.lpanalistas.com/" class="logo">
+                <img src="images/lp2.jpg" alt="LP Analistas y Consultores" />
+            </a>
+        </h1>
+    </header>
+
+    <section id="contenido">
+        {{Form::open(array('login-form', 'url'=>'/login'))}}
+            <!-- <form action="#" method="POST" id="login-form"> -->
+                <fieldset>
+                    <p>
+                        <label for="login-username">DNI</label>
+                        <!-- <input  id="username" name="username" type="text" required="required" autofocus /> -->
+                        {{Form::text('username','',array('required'=>'required','placeholder'=>'Ingrese DNI'))}}
+                    </p>
+
+                    <p>
+                        <label for="login-password">Password</label>
+                        <!-- <input type="password" id="password" required="required"/> -->
+                        {{Form::password('password','',array('required'=>'required','placeholder'=>'Ingrese clave'))}}
+                    </p>
+                    
+                    <p><a href="#">Olvide mi contraseña</a>.</p>
+                    
+                    <!-- <a href="dashboard.html" class="iniciar button">Iniciar Sesión</a> -->
+                     {{Form::submit('Iniciar Sesión', array('class' => 'iniciar button'))}}
+                </fieldset>
+            <!-- </form> -->
+        {{Form::close()}}
+    </section>
+
+    <footer>
+        <p>&copy; Copyright 2013 </p>
+        <p>
+            <a href="http://www.lpanalistas.com/">LP Analistas y Consultores.</a>
+            Todos los Derechos Reservados.
+        </p>
+    </footer>
+
+</body>
 </html>
